c0faf4bc6dad5a1da5b63e8a188063765cc3e391
greta <greta.mattarelli06@gmail.com>
2023-06-29 17:56:36 +0200

    parseInt

--- JAVASCRIPT/javascript.js
+++ JAVASCRIPT/javascript.js
@@ -12,16 +12,27 @@ let nameuser = prompt("Inserisci il tuo nome");
 let surname = prompt("Inserisci il tuo cognome");
 let prefercolor = prompt("Inserisci il tuo colore preferito");
 let number = 21;
+let number1 = parseInt(prompt("Inserisci il primo numero"));
+let number2 = parseInt(prompt("Inserisci il secondo numero"));
+
+
+// Calcolo la divisione tra i due numeri inseriti
 
 
 
-// Creazione della password
+
+
+
+// Creazione della password con variabili
 
 let password = nameuser + surname + prefercolor + number;
 
 // Stampo su consolle la variabile password
 
 console.log(password);
+
+
+
 
 
 
