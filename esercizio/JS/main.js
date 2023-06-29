6ab602df723e1b8515310bbe7dc3295c2b43c893
greta <greta.mattarelli06@gmail.com>
2023-06-29 16:50:16 +0200

    completed but error console

---
+++ esercizio/JS/main.js
@@ -0,0 +1,34 @@
+/*
+
+INZIO:
+
+// Chiedo informazioni all'utente che necessito:
+
+- Nome
+- Cognome
+- Colore preferito */
+
+let nameuser = prompt("Inserisci il tuo nome");
+let surname = prompt("Inserisci il tuo cognome");
+let prefercolor = prompt("Inserisci il tuo colore preferito");
+let number = 21;
+
+
+
+// Creazione della password
+
+let password = nameuser + surname + prefercolor + number;
+
+// Stampo su consolle la variabile password
+
+console.log(password);
+
+
+
+
+
+
+
+
+//FINE
+
